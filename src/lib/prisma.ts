2b51fb5381e20bbb84e83d8cb2e549710d1ee6ea
rouzzinasreddine <rouzzi.nasreddine@gmail.com>
2026-04-06 23:39:14 +0100

    feat: migrate to PostgreSQL Supabase

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,6 +3,6 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient({ datasourceUrl: "file:./dev.db" });
+  globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
